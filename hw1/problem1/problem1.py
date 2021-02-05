dea19668e735c48a7c33207b049f7587b1e28a3e
Baoyi Zhang <byz0325@gmail.com>
2021-02-05 15:58:51 -0600

    Feat: upload figs for 1.1

--- hw1/problem1/problem1.py
+++ hw1/problem1/problem1.py
@@ -45,20 +45,24 @@ def mix(num,w,cov,mu,d):
 ####test1
 a=categorical([1,2,3,4,5],[0.1,0.1,0.3,0.3,0.2],1000)
 plt.hist(a,bins=5)
-plt.show()
+plt.xlabel('x', fontsize=20)
+plt.ylabel('n', fontsize=20)
 ####test2
 b=unigau(0,1,1000)
 plt.hist(b,bins=30)
-plt.show()
+plt.xlabel('x', fontsize=20)
+plt.ylabel('n', fontsize=20)
 ###test3
 c=multigau([1,1],[[1,0.5],[0.5,1]],1000,2)
 plt.scatter(c[0],c[1])
-plt.show()
+plt.xlabel('x', fontsize=20)
+plt.ylabel('y', fontsize=20)
 ####test4
 cov=np.array([[1.,0.],[0.,1.]])
 cov=[cov,cov,cov,cov]
 mu=[[1.,1.],[1.,-1.],[-1.,1.],[-1.,-1.]]
 d=mix(1000,[0.25,0.25,0.25,0.25],cov,mu,[2,2,2,2])
 plt.scatter(d[0],d[1])
-plt.show()
+plt.xlabel('x', fontsize=20)
+plt.ylabel('y', fontsize=20)
 p=sum((np.power((d[0] - 0.1), 2) * np.power((d[1] - 0.2), 2))<=1)/1000
